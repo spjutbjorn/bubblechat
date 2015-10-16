23446ebf887535ab967866a6f1e6f90bb19ba37e
Spjutbjorn <Spjutbjorn@hotmail.com>
2015-10-16 14:44:17 +0200

    text inputs

--- anslagstavla/app/src/main/java/se/beamonpeople/anslagstavla/MapsActivity.java
+++ anslagstavla/app/src/main/java/se/beamonpeople/anslagstavla/MapsActivity.java
@@ -3,6 +3,9 @@ package se.beamonpeople.anslagstavla;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v4.app.FragmentActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -19,6 +22,9 @@ public class MapsActivity extends FragmentActivity {
                 .findFragmentById(R.id.map);
 
         mapFragment.getMapAsync(new MapHandler(this));
+        EditText userInput = (EditText) findViewById(R.id.editText);
+        TextView userLog = (TextView) findViewById(R.id.textView);
+        Button userSend = (Button) findViewById(R.id.sendText);
 
         DataHandler dataHandler = new DataHandler();
         new InputController(userInput, userLog,userSend, dataHandler);
